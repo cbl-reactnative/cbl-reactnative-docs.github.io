7f3c78a9ed624710f58e7818383ba1ba83df9780
Aaron LaBeau <alabeau@gmail.com>
2024-01-30 16:11:52 -0600

    update to fix deployment

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,7 +10,7 @@ const config: Config = {
   favicon: 'img/logo.png',
 
   // Set the production url of your site here
-  url: 'http://cbl-reactnative-docs.github.io',
+  url: 'https://cbl-reactnative.github.io/cbl-reactnative-docs.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
